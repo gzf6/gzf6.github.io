4098dc0cd7baae98a106bfff9753186765b7fc53
gzf6 <guanzifei@outlook.com>
2017-10-30 00:45:30 +0800

    Blog

--- js/plugin.js
+++ js/plugin.js
@@ -8,3 +8,69 @@ function burg() {
     }
     return false;
 }
+
+//qq、微信、微博图标
+function qq() {
+    let qq = document.getElementById("qq");
+    let weixin = document.getElementById("weixin");
+    let weibo = document.getElementById("weibo");
+
+    if(qq.className === "qq") {
+        qq.className = "qqs";
+    }else {
+        qq.className = "qq";
+    }
+
+    if(weixin.className === "weixins") {
+        weixin.className = "weixin";
+    }
+    if(weibo.className === "weibos") {
+        weibo.className = "weibo";
+    }
+
+    return false;
+}
+
+function weixin() {
+    let qq = document.getElementById("qq");
+    let weixin = document.getElementById("weixin");
+    let weibo = document.getElementById("weibo");
+
+
+    if(weixin.className === "weixin") {
+        weixin.className = "weixins";
+    }else {
+        weixin.className = "weixin";
+    }
+
+    if(qq.className === "qqs") {
+        qq.className = "qq";
+    }
+    if(weibo.className === "weibos") {
+        weibo.className = "weibo";
+    }
+
+    return false;
+}
+
+function weibo() {
+    let qq = document.getElementById("qq");
+    let weixin = document.getElementById("weixin");
+    let weibo = document.getElementById("weibo");
+
+
+    if(weibo.className === "weibo") {
+        weibo.className = "weibos";
+    }else {
+        weibo.className = "weibo";
+    }
+
+    if(qq.className === "qqs") {
+        qq.className = "qq";
+    }
+    if(weixin.className === "weixins") {
+        weixin.className = "weixin";
+    }
+
+    return false;
+}
